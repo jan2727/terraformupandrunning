873f6f4f5e22658368bd17b236c265f8ddc7d6ba
jan den Otter <jan@denotter.info>
2020-12-18 21:08:58 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+     ami = "ami-0bd39c806c2335b95" 
+     instance_type = "t2.micro" 
+     tags = { 
+       Name = "terraform-example"
+     }
+}
